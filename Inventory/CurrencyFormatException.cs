873ddc54b2967e2c5e8be3015c45b3385bd5960d
ffXder <fjfajardo0624@gmail.com>
2025-09-17 00:15:34 +0800

    added CurrencyClassException, NumberFormatException, StringFormatException class

---
+++ Inventory/CurrencyFormatException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class CurrencyFormatException : Exception
+    {
+        public CurrencyFormatException(string message) : base(message)
+        {
+            Console.WriteLine("This is custom Exception"); //tryy lang poo
+        }
+    }
+}
---
+++ Inventory/NumberFormatException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    public class NumberFormatException : Exception
+    {
+        public NumberFormatException(string message) : base(message)
+        {
+            Console.WriteLine("This is custom Exception"); //tryy lang poo
+        }
+    }
+}
