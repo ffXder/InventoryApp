f869cd27f1378a93cf60fd74226d893a8e2924da
ffXder <fjfajardo0624@gmail.com>
2025-11-21 01:32:56 +0800

    added ProductModel

---
+++ Inventory/ProductsModel.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes; 
+
+namespace Inventory
+{
+    // model class for products
+    public class ProductsModel
+    {
+        [BsonId]
+        [BsonElement("id")]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("productName")]
+        public string ProductName { get; set; }
+
+        [BsonElement("category")]
+        public string Category { get; set; }
+
+        [BsonElement("manufacturingDate")]
+        public string ManufacturingDate { get; set; }
+
+        [BsonElement("expirationDate")]
+        public string ExpirationDate { get; set; }
+
+        [BsonElement("sellingPrice")]
+        public double SellPrice { get; set; }
+
+        [BsonElement("quantity")]
+        public int Quantity { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+    }
+}
