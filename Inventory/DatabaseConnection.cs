8bfc8baadd379277b55a9addc8550b09c85acad5
ffXder <fjfajardo0624@gmail.com>
2025-11-21 01:31:59 +0800

    MongoDb connection class

---
+++ Inventory/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+namespace Inventory
+{
+    public class DatabaseConnection
+    {
+        private IMongoDatabase mongoDb;
+        public DatabaseConnection()
+        {
+            string url = "mongodb://localhost:27017";
+            var client = new MongoClient(url);
+            mongoDb = client.GetDatabase("InventoryDB");
+        }
+
+        public IMongoCollection<ProductsModel> GetProductsCollection()
+        {
+            // get the collection named product_inventory from the database
+            return mongoDb.GetCollection<ProductsModel>("product_inventory");
+        }
+
+    }
+}
