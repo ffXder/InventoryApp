acd7e42319041a84f6e6495c6eefd452d5c193e9
ffXder <fjfajardo0624@gmail.com>
2025-11-21 01:32:43 +0800

    ProductService Class for database operation

---
+++ Inventory/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+
+namespace Inventory
+{
+    // service class that performs database operations 
+    public class ProductService
+    {
+        private DatabaseConnection dbConnection;
+        private BindingSource bindingSource;
+        public ProductService() 
+        { 
+            dbConnection = new DatabaseConnection();
+            bindingSource = new BindingSource();
+        }
+
+        public void AddProduct(ProductsModel product)
+        {
+            var productsCollection = dbConnection.GetProductsCollection();
+            productsCollection.InsertOne(product);
+        }
+
+        public List<ProductsModel> GetAllProducts()
+        {
+            var productsCollection = dbConnection.GetProductsCollection();
+            return productsCollection.Find(_ => true).ToList();
+        }
+    }
+}
