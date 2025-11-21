631c003794665c44c873ecfd9ca3b12ad1120b51
ffXder <fjfajardo0624@gmail.com>
2025-11-21 23:02:29 +0800

    changes in design

---
+++ Inventory/FrmUpdateProduct.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Inventory
+{
+    partial class FrmUpdateProduct
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtProductName = new TextBox();
+            txtQuantity = new TextBox();
+            txtSellPrice = new TextBox();
+            cbCategory = new ComboBox();
+            dtPickerMfgDate = new DateTimePicker();
+            dtPickerExpDate = new DateTimePicker();
+            label9 = new Label();
+            richTxtDescription = new RichTextBox();
+            btnUpdateProduct = new Button();
+            label8 = new Label();
+            txtProductId = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(144, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Update Product";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(21, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Product";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(21, 145);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Category";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(21, 186);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Mfg. Date";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(21, 230);
+            label5.Name = "label5";
+            label5.Size = new Size(72, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Exp. Date";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(21, 277);
+            label6.Name = "label6";
+            label6.Size = new Size(35, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Qty.";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(21, 319);
+            label7.Name = "label7";
+            label7.Size = new Size(69, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Sell Price";
+            // 
+            // txtProductName
+            // 
+            txtProductName.BorderStyle = BorderStyle.FixedSingle;
+            txtProductName.Cursor = Cursors.Hand;
+            txtProductName.Location = new Point(119, 107);
+            txtProductName.Name = "txtProductName";
+            txtProductName.Size = new Size(252, 23);
+            txtProductName.TabIndex = 7;
+            // 
+            // txtQuantity
+            // 
+            txtQuantity.BorderStyle = BorderStyle.FixedSingle;
+            txtQuantity.Cursor = Cursors.Hand;
+            txtQuantity.Location = new Point(119, 277);
+            txtQuantity.Name = "txtQuantity";
+            txtQuantity.Size = new Size(252, 23);
+            txtQuantity.TabIndex = 8;
+            // 
+            // txtSellPrice
+            // 
+            txtSellPrice.BorderStyle = BorderStyle.FixedSingle;
+            txtSellPrice.Cursor = Cursors.Hand;
+            txtSellPrice.Location = new Point(119, 319);
+            txtSellPrice.Name = "txtSellPrice";
+            txtSellPrice.Size = new Size(252, 23);
+            txtSellPrice.TabIndex = 9;
+            // 
+            // cbCategory
+            // 
+            cbCategory.Cursor = Cursors.Hand;
+            cbCategory.FormattingEnabled = true;
+            cbCategory.Location = new Point(119, 145);
+            cbCategory.Name = "cbCategory";
+            cbCategory.Size = new Size(252, 23);
+            cbCategory.TabIndex = 10;
+            // 
+            // dtPickerMfgDate
+            // 
+            dtPickerMfgDate.Cursor = Cursors.Hand;
+            dtPickerMfgDate.Location = new Point(119, 186);
+            dtPickerMfgDate.Name = "dtPickerMfgDate";
+            dtPickerMfgDate.Size = new Size(252, 23);
+            dtPickerMfgDate.TabIndex = 11;
+            // 
+            // dtPickerExpDate
+            // 
+            dtPickerExpDate.Cursor = Cursors.Hand;
+            dtPickerExpDate.Location = new Point(119, 230);
+            dtPickerExpDate.Name = "dtPickerExpDate";
+            dtPickerExpDate.Size = new Size(252, 23);
+            dtPickerExpDate.TabIndex = 12;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(404, 72);
+            label9.Name = "label9";
+            label9.Size = new Size(85, 20);
+            label9.TabIndex = 14;
+            label9.Text = "Description";
+            // 
+            // richTxtDescription
+            // 
+            richTxtDescription.Location = new Point(404, 107);
+            richTxtDescription.Name = "richTxtDescription";
+            richTxtDescription.Size = new Size(273, 194);
+            richTxtDescription.TabIndex = 15;
+            richTxtDescription.Text = "";
+            // 
+            // btnUpdateProduct
+            // 
+            btnUpdateProduct.Cursor = Cursors.Hand;
+            btnUpdateProduct.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnUpdateProduct.Location = new Point(556, 319);
+            btnUpdateProduct.Name = "btnUpdateProduct";
+            btnUpdateProduct.Size = new Size(121, 47);
+            btnUpdateProduct.TabIndex = 16;
+            btnUpdateProduct.Text = "Update Product";
+            btnUpdateProduct.UseVisualStyleBackColor = true;
+            btnUpdateProduct.Click += btnUpdateProduct_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(21, 72);
+            label8.Name = "label8";
+            label8.Size = new Size(79, 20);
+            label8.TabIndex = 17;
+            label8.Text = "Product ID";
+            // 
+            // txtProductId
+            // 
+            txtProductId.BorderStyle = BorderStyle.FixedSingle;
+            txtProductId.Cursor = Cursors.Hand;
+            txtProductId.Enabled = false;
+            txtProductId.Location = new Point(119, 69);
+            txtProductId.Name = "txtProductId";
+            txtProductId.Size = new Size(252, 23);
+            txtProductId.TabIndex = 18;
+            // 
+            // FrmUpdateProduct
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(689, 378);
+            Controls.Add(txtProductId);
+            Controls.Add(label8);
+            Controls.Add(btnUpdateProduct);
+            Controls.Add(richTxtDescription);
+            Controls.Add(label9);
+            Controls.Add(dtPickerExpDate);
+            Controls.Add(dtPickerMfgDate);
+            Controls.Add(cbCategory);
+            Controls.Add(txtSellPrice);
+            Controls.Add(txtQuantity);
+            Controls.Add(txtProductName);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "FrmUpdateProduct";
+            Text = "Update Product";
+            Load += FrmUpdateProduct_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtProductName;
+        private TextBox txtQuantity;
+        private TextBox txtSellPrice;
+        private ComboBox cbCategory;
+        private DateTimePicker dtPickerMfgDate;
+        private DateTimePicker dtPickerExpDate;
+        private Label label9;
+        private RichTextBox richTxtDescription;
+        private Button btnUpdateProduct;
+        private Label label8;
+        private TextBox txtProductId;
+    }
+}
