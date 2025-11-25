6a553e91d78cc5851ffb8ad56e9d33d75a445ca2
ffXder <fjfajardo0624@gmail.com>
2025-11-25 20:44:36 +0800

    design changes

--- Inventory/FrmInventory.Designer.cs
+++ Inventory/FrmInventory.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmInventory));
             gridViewProductList = new DataGridView();
             btnRefresh = new Button();
@@ -44,9 +44,9 @@
             txtSearch = new TextBox();
             label3 = new Label();
             panel1 = new Panel();
-            button2 = new Button();
+            btnStockOut = new Button();
             numStock = new NumericUpDown();
-            button1 = new Button();
+            btnStockIn = new Button();
             panel2 = new Panel();
             btnClose = new Button();
             pictureBox1 = new PictureBox();
@@ -62,36 +62,36 @@
             gridViewProductList.AllowUserToAddRows = false;
             gridViewProductList.AllowUserToDeleteRows = false;
             gridViewProductList.BackgroundColor = SystemColors.ActiveBorder;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = Color.SteelBlue;
-            dataGridViewCellStyle1.Font = new Font("Poppins SemiBold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle1.ForeColor = Color.White;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            gridViewProductList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.SteelBlue;
+            dataGridViewCellStyle4.Font = new Font("Poppins SemiBold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle4.ForeColor = Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            gridViewProductList.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             gridViewProductList.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             gridViewProductList.Cursor = Cursors.Hand;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = SystemColors.Control;
-            dataGridViewCellStyle2.Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
-            gridViewProductList.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = SystemColors.Control;
+            dataGridViewCellStyle5.Font = new Font("Poppins", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle5.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.False;
+            gridViewProductList.DefaultCellStyle = dataGridViewCellStyle5;
             gridViewProductList.EnableHeadersVisualStyles = false;
             gridViewProductList.Location = new Point(200, 142);
             gridViewProductList.Name = "gridViewProductList";
             gridViewProductList.ReadOnly = true;
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = SystemColors.Control;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle3.ForeColor = Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
-            gridViewProductList.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = SystemColors.Control;
+            dataGridViewCellStyle6.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle6.ForeColor = Color.Black;
+            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.True;
+            gridViewProductList.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
             gridViewProductList.Size = new Size(1084, 595);
             gridViewProductList.TabIndex = 0;
             gridViewProductList.CellContentClick += gridViewProductList_CellContentClick;
@@ -217,9 +217,9 @@
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             panel1.BackColor = Color.SteelBlue;
             panel1.BackgroundImageLayout = ImageLayout.None;
-            panel1.Controls.Add(button2);
+            panel1.Controls.Add(btnStockOut);
             panel1.Controls.Add(numStock);
-            panel1.Controls.Add(button1);
+            panel1.Controls.Add(btnStockIn);
             panel1.Controls.Add(btnUpdate);
             panel1.Controls.Add(btnAdd);
             panel1.Controls.Add(btnDelete);
@@ -228,16 +228,17 @@
             panel1.Size = new Size(184, 687);
             panel1.TabIndex = 12;
             // 
-            // button2
+            // btnStockOut
             // 
-            button2.BackColor = Color.White;
-            button2.Font = new Font("Poppins SemiBold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button2.Location = new Point(23, 336);
-            button2.Name = "button2";
-            button2.Size = new Size(131, 39);
-            button2.TabIndex = 14;
-            button2.Text = "Stock Out";
-            button2.UseVisualStyleBackColor = false;
+            btnStockOut.BackColor = Color.White;
+            btnStockOut.Font = new Font("Poppins SemiBold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnStockOut.Location = new Point(23, 336);
+            btnStockOut.Name = "btnStockOut";
+            btnStockOut.Size = new Size(131, 39);
+            btnStockOut.TabIndex = 14;
+            btnStockOut.Text = "Stock Out";
+            btnStockOut.UseVisualStyleBackColor = false;
+            btnStockOut.Click += btnStockOut_Click;
             // 
             // numStock
             // 
@@ -247,16 +248,17 @@
             numStock.Size = new Size(131, 25);
             numStock.TabIndex = 13;
             // 
-            // button1
+            // btnStockIn
             // 
-            button1.BackColor = Color.White;
-            button1.Font = new Font("Poppins SemiBold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(23, 291);
-            button1.Name = "button1";
-            button1.Size = new Size(131, 39);
-            button1.TabIndex = 6;
-            button1.Text = "Stock In";
-            button1.UseVisualStyleBackColor = false;
+            btnStockIn.BackColor = Color.White;
+            btnStockIn.Font = new Font("Poppins SemiBold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnStockIn.Location = new Point(23, 291);
+            btnStockIn.Name = "btnStockIn";
+            btnStockIn.Size = new Size(131, 39);
+            btnStockIn.TabIndex = 6;
+            btnStockIn.Text = "Stock In";
+            btnStockIn.UseVisualStyleBackColor = false;
+            btnStockIn.Click += btnStockIn_Click;
             // 
             // panel2
             // 
@@ -274,8 +276,9 @@
             // 
             btnClose.BackColor = Color.Firebrick;
             btnClose.Cursor = Cursors.Hand;
-            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.FlatStyle = FlatStyle.Popup;
             btnClose.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClose.ForeColor = Color.White;
             btnClose.Location = new Point(1243, 12);
             btnClose.Name = "btnClose";
             btnClose.Size = new Size(42, 23);
@@ -339,11 +342,11 @@
         private TextBox txtSearch;
         private Label label3;
         private Panel panel1;
-        private Button button1;
+        private Button btnStockIn;
         private NumericUpDown numStock;
         private Panel panel2;
         private PictureBox pictureBox1;
         private Button btnClose;
-        private Button button2;
+        private Button btnStockOut;
     }
 }
