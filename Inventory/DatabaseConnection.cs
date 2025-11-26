5d6964b9390118b212c900c47d602c16316ec06d
ffXder <fjfajardo0624@gmail.com>
2025-11-26 21:03:46 +0800

    added try catch

--- Inventory/DatabaseConnection.cs
+++ Inventory/DatabaseConnection.cs
@@ -7,12 +7,18 @@ namespace Inventory
         private IMongoDatabase mongoDb;
         public DatabaseConnection()
         {
-            Env.Load();
-            string url = Environment.GetEnvironmentVariable("MONGOATLAS_URI"); //to get the env variable
-            var client = new MongoClient(url);
-            mongoDb = client.GetDatabase("InventoryDB");
+            try
+            {
+                Env.Load();
+                string url = Environment.GetEnvironmentVariable("MONGOATLAS_URI"); //to get the env variable
+                var client = new MongoClient(url);
+                mongoDb = client.GetDatabase("InventoryDB");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error connecting to database: " + e.Message);
+            }
         }
-
         public IMongoCollection<ProductsModel> GetProductsCollection()
         {
             // get the collection named product_inventory from the database
